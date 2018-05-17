b1d01bcec69b1e9e07596cd990e17a56e3d0e80b
Tobias Kiel <tobias.kiel@ericsson.com>
2018-05-17 14:35:43 +0200

    Pimping start page abit

--- fireboard/src/components/games-screen.js
+++ fireboard/src/components/games-screen.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-
-import Firebase from '../firebase/firebase-api';
+import React, { Component } from "react";
+import { View, Text, Button } from "react-native";
+import Panel from './panel'
+import Firebase from "../firebase/firebase-api";
 
 class GamesScreen extends Component {
   componentDidMount() {
@@ -14,9 +14,25 @@ class GamesScreen extends Component {
 
   render() {
     return (
-      <View>
-        <Text>I'm the games screen</Text>
-        <Button title='New game' onPress={() => this.props.navigation.navigate("CreateGame", {title: 'Create new game'})}></Button>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        <Panel style={{ flex: 0.5 }}>
+          <Text style={{ color: "white",borderBottomWidth:1,borderBottomColor:'black', textAlign: "center", fontSize: 20 }}>
+            Somewhat ongoing Games:
+          </Text>
+        </Panel>
+        <Panel style={{ flex: 0.5}}>
+          <Text style={{ color: "white", borderBottomWidth:1,borderBottomColor:'black',textAlign: "center", fontSize: 20 }}>
+            Winner and losers:
+          </Text>
+          <Button
+            title="New game"
+            onPress={() =>
+              this.props.navigation.navigate("CreateGame", {
+                title: "Create new game"
+              })
+            }
+          />
+        </Panel>
       </View>
     );
   }
