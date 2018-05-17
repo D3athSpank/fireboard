639bd4caacfae40bf7c95cbe9e35b4224d299c3d
Tobias Kiel <tobias.kiel@ericsson.com>
2018-05-17 14:30:37 +0200

    New logo and splash Add Player Ui complete

--- fireboard/src/components/create-game-screen.js
+++ fireboard/src/components/create-game-screen.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Panel from "../components/panel";
 import { ActivityIndicator } from "react-native";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Picker } from "react-native";
 
 export default class CreateGameScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -11,43 +11,65 @@ export default class CreateGameScreen extends Component {
       backgroundColor: "white"
     }
   });
-
-	getPickerItems(){
-		const players = [
-			{name: "Rikard", avatar: "http://i0.kym-cdn.com/entries/icons/original/000/003/549/Dolan.jpg"}, 
-			{name: "Knugen", avatar: "http://i0.kym-cdn.com/entries/icons/original/000/003/549/Dolan.jpg"}, 
-			{name: "Foppa", avatar: "http://i0.kym-cdn.com/entries/icons/original/000/003/549/Dolan.jpg"}]
-
-		return players.map((p, i) => {
-			return <Picker.Item key={i} label={p.name} value={p} />
-		});
+constructor(props){
+	super(props);
+	this.state={
+		playerOne:null,
+		playerTwo:null
 	}
+
+}
+  getPickerItems() {
+    const players = [
+      {
+        name: "Rikard",
+        avatar:
+          "http://i0.kym-cdn.com/entries/icons/original/000/003/549/Dolan.jpg"
+      },
+      {
+        name: "Knugen",
+        avatar:
+          "http://i0.kym-cdn.com/entries/icons/original/000/003/549/Dolan.jpg"
+      },
+      {
+        name: "Foppa",
+        avatar:
+          "http://i0.kym-cdn.com/entries/icons/original/000/003/549/Dolan.jpg"
+      }
+    ];
+
+    return players.map((p, i) => {
+      return <Picker.Item key={i} label={p.name} value={p} />;
+    });
+  }
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: "white" }}>
         <Panel style={{ justifyContent: "center", alignItems: "center" }}>
-				<Text>Player 1</Text>
-				<Picker
-					selectedValue={this.state.playerOne || null}
-					style={{ height: 50, width: 150 }}
-					onValueChange={(value) => {
-						this.setState({playerOne: value});
-					}}
-				>
-					{this.getPickerItems()}
-				</Picker>
+          <Text>Player 1</Text>
+          <Picker
+            selectedValue={this.state.playerOne || null}
+            style={{ height: 50, width: 150 }}
+            onValueChange={value => {
+              this.setState({ playerOne: value });
+            }}
+          >
+            {this.getPickerItems()}
+          </Picker>
 
-				<Text>Player 2</Text>
-				<Picker
-					selectedValue={this.state.playerTwo || null}
-					style={{ height: 50, width: 150 }}
-					onValueChange={(value) => {
-						this.setState({playerTwo: value});
-					}}
-				>
-					{this.getPickerItems()}
-				</Picker>
-				<Text>{this.state.playerOne && this.state.playerOne.name || "nope"}</Text>
+          <Text>Player 2</Text>
+          <Picker
+            selectedValue={this.state.playerTwo || null}
+            style={{ height: 50, width: 150 }}
+            onValueChange={value => {
+              this.setState({ playerTwo: value });
+            }}
+          >
+            {this.getPickerItems()}
+          </Picker>
+          <Text>
+            {(this.state.playerOne && this.state.playerOne.name) || "nope"}
+          </Text>
 
           {/* Should be replaced with circular + button menu */}
           <Button
