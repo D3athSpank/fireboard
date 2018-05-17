da460a9816d04191fd27ec32256b9fa9a8232c2b
Rikard Jurstrand <klaxel@live.se>
2018-05-17 16:06:48 +0200

    player selector in own component

--- fireboard/src/components/games-screen.js
+++ fireboard/src/components/games-screen.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
-
-import Firebase from '../firebase/firebase-api';
-=======
 import React, { Component } from "react";
 import { View, Text, Button } from "react-native";
 import Panel from './panel'
 import Firebase from "../firebase/firebase-api";
->>>>>>> 5c27a76a1b8e0c582ef22899990c5abc8b28ac95
 
-export default class GamesScreen extends Component {
+class GamesScreen extends Component {
   componentDidMount() {
     Firebase.listenToPlayers(games => console.log('PL', games));
     Firebase.listenToGames(games => console.log('GA', games));
@@ -22,18 +15,6 @@ export default class GamesScreen extends Component {
   }
   render() {
     return (
-<<<<<<< HEAD
-      <View>
-        <Text>I'm the games screen</Text>
-        <Button
-          title="New game"
-          onPress={() =>
-            this.props.navigation.navigate('CreateGame', {
-              title: 'Create new game'
-            })
-          }
-        />
-=======
       <View style={{ flex: 1, backgroundColor: "white" }}>
         <Panel style={{ flex: 0.5 }}>
           <Text style={{ color: "white",borderBottomWidth:1,borderBottomColor:'black', textAlign: "center", fontSize: 20 }}>
@@ -53,13 +34,9 @@ export default class GamesScreen extends Component {
             }
           />
         </Panel>
->>>>>>> 5c27a76a1b8e0c582ef22899990c5abc8b28ac95
       </View>
     );
   }
 }
-<<<<<<< HEAD
-=======
 
 export default GamesScreen;
->>>>>>> 5c27a76a1b8e0c582ef22899990c5abc8b28ac95
---
+++ fireboard/src/components/player-selector.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Panel from "../components/panel";
+import { Text, Picker, Image } from "react-native";
+// import { ActivityIndicator } from "react-native";
+// import { View, Text, Button, Picker, Image } from "react-native";
+// import { FloatingAction } from 'react-native-floating-action';
+
+export default class PlayerSelector extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedPlayer: null
+		}
+	}
+
+	getPickerItems() {
+		console.log("pickerItems", this.props.players)
+		return this.props.players.map((p, i) => {
+			return <Picker.Item key={i} label={p.nick} value={p.id} />
+		});
+	}
+
+	render() {
+		
+		return (
+			<Panel style={{ flex: 0.5 }}>
+				
+				<Text style={{ color: "white", textAlign: "center", fontSize: 38 }}>Combatant #{this.props.isPlayerOne ? "1" : "2"}</Text>
+				<Picker
+					selectedValue={this.props.selectedPlayer && this.props.selectedPlayer.id || null}
+					style={{ height: 50, width: "100%", color: "white" }}
+					textStyle={{fontSize: 20}}
+					onValueChange={(value) => {
+						this.props.onPlayerSelected(value, this.props.isPlayerOne);
+					}}
+				>
+					<Picker.Item label={`Select player ${this.props.isPlayerOne ? "one" : "two"}`} value={""} />
+					{this.getPickerItems()}
+				</Picker>
+				{/* {this.props.selectedPlayer && 
+					<Image
+						style={{
+							flex: 1,
+							width: null,
+							height: null,
+							resizeMode: "contain"
+						}}
+						source={this.state.selectedPlayer.avatar ? {uri: this.state.selectedPlayer.avatar} : require("../img/user.png")}
+					/>} */}
+			</Panel>
+		)
+	}
+}
+	
