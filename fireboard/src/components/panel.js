c028a86f5cedd09e9d902c65e426bab8f202d176
Rikard Jurstrand <klaxel@live.se>
2018-05-17 13:21:31 +0200

    panel padding

--- fireboard/src/components/panel.js
+++ fireboard/src/components/panel.js
@@ -3,12 +3,12 @@ import { View, Text } from 'react-native'
 
 export default class Panel extends Component {
 	constructor(props) {
-    super(props)
+    	super(props)
 	}
 	
 	render() {
 		return (
-			<View style={{margin: 10, backgroundColor: '#007F00', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+			<View style={{margin: 10, padding: 10, backgroundColor: '#007F00', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
 				{this.props.children}
 			</View>
 		);
